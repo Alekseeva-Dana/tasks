7ad5d0b046c0562ca7b6873fa2614f89275abd2c
Alekseeva-Dana <s02240056@gse.cs.msu.ru>
2025-11-09 13:19:11 +0300

    Руденко 8.18 пункт i

---
+++ homework/lesson_14/2_i.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int main() {
+    /*первая часть: pr1 arg1 < f.dat*/
+    int in_fd = open("f.dat", O_RDONLY);
+    if (in_fd == -1) {
+        perror("open f.dat");
+        exit(1);
+    }
+
+    pid_t pid1 = fork();
+    if (pid1 == 0) {
+        dup2(in_fd, STDIN_FILENO);
+        close(in_fd);
+        execlp("pr1", "pr1", "arg1", NULL);
+        perror("execlp pr1");
+        exit(1);
+    }
+    close(in_fd);
+    waitpid(pid1, NULL, 0);  /*ждем завершения pr1*/
+
+    /*вторая часть: pr2 | pr3 >> f.res */
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    /*открываем файл для дописывания*/
+    int out_fd = open("f.res", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (out_fd == -1) {
+        perror("open f.res");
+        exit(1);
+    }
+
+    pid_t pid2 = fork();
+    if (pid2 == 0) {
+        /*pr2 - пишет в pipe*/
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]); close(pipefd[1]);
+        close(out_fd);
+        execlp("pr2", "pr2", NULL);
+        perror("execlp pr2");
+        exit(1);
+    }
+
+    pid_t pid3 = fork();
+    if (pid3 == 0) {
+        /*pr3 - читает из pipe, дописывает в файл*/
+        dup2(pipefd[0], STDIN_FILENO);
+        dup2(out_fd, STDOUT_FILENO);
+        close(pipefd[0]); close(pipefd[1]);
+        close(out_fd);
+        execlp("pr3", "pr3", NULL);
+        perror("execlp pr3");
+        exit(1);
+    }
+
+    close(pipefd[0]); close(pipefd[1]);
+    close(out_fd);
+    
+    /*ждем оставшиеся процессы*/
+    waitpid(pid2, NULL, 0);
+    waitpid(pid3, NULL, 0);
+    
+    return 0;
+}
