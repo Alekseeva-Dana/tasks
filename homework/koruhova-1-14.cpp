ecd49fb9760f4ac152cc0e66f1a30c164380b335
Alekseeva-Dana <s02240056@gmail.com>
2026-02-18 21:33:49 +0300

    1.14

---
+++ homework/koruhova-1-14.cpp
@@ -0,0 +1,13 @@
+//пункт а
+Ball(const char* color, int diameter)
+Ball(int diameter)
+добавление функции smallest
+
+//пункт б
+класс tiger должен иметь:
+	-конструктор по умолчанию
+	-конструктор tiger(const char* name)
+	-методы setname, setcolor, setweight, print()
+	-статическое поле count
+	-дружественную функцию less
+less(tiger& t2, tiger& t1)
